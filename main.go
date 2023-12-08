2a5d81729f38cf1e99e5861cef8fee7057c40ad2
Niclas Tillman <tillman.niclas@gmail.com>
2023-12-08 11:47:01 +0100

    Add optional https mode
    Change-Id: I6e827710a2ff7998f49dbe93886cfd5e982b9db6

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -49,11 +50,33 @@ func main() {
 
 	// Set the port for the server to listen on
 	port := 8080
-
-	// Start the web server and listen on the specified port
 	fmt.Printf("Server is listening on port %d...\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-	if err != nil {
-		fmt.Println("Error starting the server:", err)
+
+	// Define command line arguments
+	https := flag.Bool("https", false, "Sets the webserver in https only mode")
+
+	// Parse command line arguments
+	flag.Parse()
+
+	if *https {
+		fmt.Printf("Starting the server in https mode. Go to https://localhost:%d", port)
+
+		// Specify paths to TLS certificate and private key files
+		certFile := "./security/cert.pem"
+		keyFile := "./security/key.pem"
+
+		// Start the web server with HTTPS and listen on the specified port
+		error := http.ListenAndServeTLS(fmt.Sprintf(":%d", port), certFile, keyFile, nil)
+		if error != nil {
+			fmt.Println("Error starting the server:", error)
+		}
+	} else {
+		fmt.Printf("Starting the server in http mode. Go to http://localhost:%d", port)
+
+		// Start the web server and listen on the specified port
+		error := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+		if error != nil {
+			fmt.Println("Error starting the server:", error)
+		}
 	}
 }

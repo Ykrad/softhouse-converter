48d55a179ab3b6537d12ad50ec4669bd528089e1
Niclas Tillman <tillman.niclas@gmail.com>
2023-12-11 15:43:02 +0100

    Add functionality for uploading a file
    As well as displaying it
    
    Change-Id: Ic6e81d99853b1b0d0e4d4264ef78d90b0c762c46

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 )
 
 // Page is a struct to represent the data that will be rendered in the HTML template
 type Page struct {
-	Title string
+	Title               string
+	UploadedFileContent string
 }
 
 func handler(writer http.ResponseWriter, request *http.Request) {
@@ -18,13 +20,43 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	if request.Method != "GET" {
+	if request.Method != "GET" && request.Method != "POST" {
 		http.Error(writer, "Method is not supported.", http.StatusNotFound)
 		return
 	}
 
 	page := Page{
-		Title: "PSV to XML converter",
+		Title:               "PSV to XML converter",
+		UploadedFileContent: "",
+	}
+
+	if request.Method == http.MethodPost {
+		// Parse the form data
+		err := request.ParseMultipartForm(10 << 20) // 10 MB limit
+		if err != nil {
+			http.Error(writer, "Unable to parse form", http.StatusBadRequest)
+			return
+		}
+
+		// Get the file from the form
+		file, handler, err := request.FormFile("file")
+		if err != nil {
+			http.Error(writer, "Unable to get file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		fmt.Println("Received file: ", handler.Filename)
+
+		// Read the content of the uploaded file
+		content, err := io.ReadAll(file)
+		if err != nil {
+			http.Error(writer, "Unable to read file content", http.StatusInternalServerError)
+			return
+		}
+
+		// Add content to page template data
+		page.UploadedFileContent = string(content)
 	}
 
 	// Parse the HTML template file

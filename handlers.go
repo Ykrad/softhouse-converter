2560ff4af18828fdd30c1049fdacdf0ad29dc20c
Niclas Tillman <tillman.niclas@gmail.com>
2023-12-14 14:22:12 +0100

    Refactor http handlers to their own file
    Change-Id: I65d6e26a55514d7024495b7ee38e0ceaf20faa18

---
+++ handlers.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+// Page is a struct to represent the data that will be rendered in the HTML template
+type Page struct {
+	Title               string
+	UploadedFileContent string
+}
+
+// Handles path "/"
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	// Validate
+	if request.URL.Path != "/" {
+		http.Error(writer, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if request.Method != "GET" && request.Method != "POST" {
+		http.Error(writer, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	// Initialize page struct
+	page := Page{
+		Title:               "PSV to XML converter",
+		UploadedFileContent: "",
+	}
+
+	if request.Method == http.MethodPost {
+		// Parse the form data
+		err := request.ParseMultipartForm(10 << 20) // 10 MB limit
+		if err != nil {
+			http.Error(writer, "Unable to parse form", http.StatusBadRequest)
+			return
+		}
+
+		// Get the file from the form
+		file, handler, err := request.FormFile("file")
+		if err != nil {
+			http.Error(writer, "Unable to get file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		fmt.Println("Received file: ", handler.Filename)
+
+		// Read the content of the uploaded file
+		content, err := io.ReadAll(file)
+		if err != nil {
+			http.Error(writer, "Unable to read file content", http.StatusInternalServerError)
+			return
+		}
+
+		// Add content to page template data
+		page.UploadedFileContent = string(content)
+	}
+
+	// Parse the HTML template file
+	indexTemplate, error := template.ParseFiles("./index.html")
+	if error != nil {
+		http.Error(writer, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with the data and write the result to the response writer
+	error = indexTemplate.Execute(writer, page)
+	if error != nil {
+		http.Error(writer, error.Error(), http.StatusInternalServerError)
+	}
+}
--- main.go
+++ main.go
@@ -3,82 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html/template"
-	"io"
 	"net/http"
 )
-
-// Page is a struct to represent the data that will be rendered in the HTML template
-type Page struct {
-	Title               string
-	UploadedFileContent string
-}
-
-func handler(writer http.ResponseWriter, request *http.Request) {
-	if request.URL.Path != "/" {
-		http.Error(writer, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if request.Method != "GET" && request.Method != "POST" {
-		http.Error(writer, "Method is not supported.", http.StatusNotFound)
-		return
-	}
-
-	page := Page{
-		Title:               "PSV to XML converter",
-		UploadedFileContent: "",
-	}
-
-	if request.Method == http.MethodPost {
-		// Parse the form data
-		err := request.ParseMultipartForm(10 << 20) // 10 MB limit
-		if err != nil {
-			http.Error(writer, "Unable to parse form", http.StatusBadRequest)
-			return
-		}
-
-		// Get the file from the form
-		file, handler, err := request.FormFile("file")
-		if err != nil {
-			http.Error(writer, "Unable to get file", http.StatusBadRequest)
-			return
-		}
-		defer file.Close()
-
-		fmt.Println("Received file: ", handler.Filename)
-
-		// Read the content of the uploaded file
-		content, err := io.ReadAll(file)
-		if err != nil {
-			http.Error(writer, "Unable to read file content", http.StatusInternalServerError)
-			return
-		}
-
-		// Add content to page template data
-		page.UploadedFileContent = string(content)
-	}
-
-	// Parse the HTML template file
-	indexTemplate, error := template.ParseFiles("./index.html")
-	if error != nil {
-		http.Error(writer, error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Execute the template with the data and write the result to the response writer
-	error = indexTemplate.Execute(writer, page)
-	if error != nil {
-		http.Error(writer, error.Error(), http.StatusInternalServerError)
-	}
-}
 
 func main() {
 	// Serve static files from the "static" directory
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Register the handler function for a specific route
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
 
 	// Set the port for the server to listen on
 	port := 8080

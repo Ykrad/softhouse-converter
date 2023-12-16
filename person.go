ee7f2fc1e0ce849964c815f6b2344a7eeb6d32a3
Niclas Tillman <tillman.niclas@gmail.com>
2023-12-16 17:21:52 +0100

    Move person structs to own file
    Change-Id: I3c0a7263f9500a9ee1bf888ba3893d7726629beb

---
+++ person.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type Phone struct {
+	mobile   string
+	landline string
+}
+
+func (phone Phone) toXML() string {
+	return fmt.Sprintf("<phone><mobile>%s</mobile><landline>%s</landline></phone>",
+		phone.mobile, phone.landline)
+}
+
+type Address struct {
+	street   string
+	city     string
+	areaCode string
+}
+
+func (address Address) toXML() string {
+	return fmt.Sprintf("<address><street>%s</street><city>%s</city><areaCode>%s</areaCode></address>",
+		address.street, address.city, address.areaCode)
+}
+
+type Person struct {
+	firstName          string
+	lastName           string
+	phone              Phone
+	phoneInitialized   bool
+	address            Address
+	addressInitialized bool
+}
+
+func (person Person) toXML() string {
+	phone := ""
+	if person.phoneInitialized {
+		phone = person.phone.toXML()
+	}
+
+	address := ""
+	if person.addressInitialized {
+		address = person.address.toXML()
+	}
+
+	return fmt.Sprintf("<person><firstname>%s</firstname><lastname>%s</lastname>%s%s</person>",
+		person.firstName, person.lastName, phone, address)
+}
